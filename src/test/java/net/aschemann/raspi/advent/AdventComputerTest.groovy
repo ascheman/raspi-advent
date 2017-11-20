e6d16de43d2695ef30dbf0d8b135178a5424934c
Gerd Aschemann <gerd@aschemann.net>
2017-11-20 19:34:57 +0100

    Add some test cases

--- src/test/java/net/aschemann/raspi/advent/AdventComputerTest.groovy
+++ src/test/java/net/aschemann/raspi/advent/AdventComputerTest.groovy
@@ -1,5 +1,6 @@
 package net.aschemann.raspi.advent
 
+import org.joda.time.LocalDate
 import org.junit.Test
 
 
@@ -8,8 +9,38 @@ import org.junit.Test
  */
 class AdventComputerTest {
     @Test
-    void "test now" () {
-        AdventComputer ac = new AdventComputer()
+    void "test first of advent" () {
+        AdventComputer ac = new AdventComputer(new LocalDate("2017-12-03"))
         assert 1 == ac.current()
+    }
+
+    @Test
+    void "test before first of advent" () {
+        AdventComputer ac = new AdventComputer(new LocalDate("2017-12-02"))
+        assert 0 == ac.current()
+    }
+
+    @Test
+    void "test christmas 2017 (exactly on 4th advent)" () {
+        AdventComputer ac = new AdventComputer(new LocalDate("2017-12-24"))
+        assert 4 == ac.current()
+    }
+
+    @Test
+    void "after christmas 2017" () {
+        AdventComputer ac = new AdventComputer(new LocalDate("2017-12-25"))
+        assert 0 == ac.current()
+    }
+
+    @Test
+    void "test second of advent" () {
+        AdventComputer ac = new AdventComputer(new LocalDate("2017-12-10"))
+        assert 2 == ac.current()
+    }
+
+    @Test
+    void "test fourth of advent in 2016" () {
+        AdventComputer ac = new AdventComputer(new LocalDate("2016-12-21"))
+        assert 4 == ac.current()
     }
 }
